12362bee370b4ba300062c5a26ce3140bfe1c329
Dan <neveloffd@gmail.com>
2021-02-08 18:00:41 -0500

    post create working kinda

--- wayfarer/main_app/urls.py
+++ wayfarer/main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/login', views.login, name='login'),
     path('accounts/logout', views.logout, name='logout'),
     path('011100110111011101101111011100100110010001100110011010010111001101101000/', views.textscroll, name='textscroll'),
+    path('/post_create', views.post_create, name='post_create'),
     path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:post_id>/delete/', views.post_delete, name='post_delete'),
     path('profile/<int:profile_id>/', views.profile, name='profile'),
