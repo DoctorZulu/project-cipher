8d951576aef106b2be80d9366f03b4c39930e949
Dan <neveloffd@gmail.com>
2021-02-15 17:33:00 -0500

    changing collectstatic

--- wayfarer/wayfarer_project/settings.py
+++ wayfarer/wayfarer_project/settings.py
@@ -16,7 +16,7 @@ import django_on_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = "/static/"
 
 django_on_heroku.settings(locals())
